2383ef22dc93160529d59df3f4cdc85a7ef20c43
daeniz <dennis@schmock.eu>
2016-09-15 21:49:26 +0200

    Completed entities, added simple test strings

--- src/main/java/entity/Task.java
+++ src/main/java/entity/Task.java
@@ -44,6 +44,7 @@ public class Task implements Serializable {
     }
 
     
+    //Getters and setters below this point
     public Integer getId() {
         return id;
     }
@@ -52,30 +53,6 @@ public class Task implements Serializable {
         this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Task)) {
-            return false;
-        }
-        Task other = (Task) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.Task[ id=" + id + " ]";
-    }
 
     /**
      * @return the name
